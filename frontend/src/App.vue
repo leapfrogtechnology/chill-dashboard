36142b04f918f95200724423eb38a06e7ebbe3ae
Bishal Shrestha <bishal@adf.local>
2017-05-01 20:20:10 +0545

    CHILL-1 Initial setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div id="wrapper">
+      <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+          <a class="navbar-brand">Chill</a>
+        </div>
+
+        <div class="navbar-default sidebar" role="navigation">
+          <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+              <li>
+                <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div id="page-wrapper">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../src/assets/css/custom.css'
+import '../node_modules/font-awesome/css/font-awesome.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
