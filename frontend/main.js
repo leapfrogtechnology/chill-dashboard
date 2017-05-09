d8005ee90bbd251b148466a83fb8353231966a2b
Bishal Shrestha <bishal@adf.local>
2017-05-09 06:03:25 +0545

    CHILL-11 Remove static folder

--- frontend/main.js
+++ frontend/main.js
@@ -2,9 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+
+import router from './router/index'
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../src/assets/css/custom.css'
+import './assets/css/custom.css'
 import '../node_modules/font-awesome/css/font-awesome.css'
 
 Vue.config.productionTip = false
