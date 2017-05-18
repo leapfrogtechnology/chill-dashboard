30cd003b107a8bb8a7cfb4ba4e280a7be3461e45
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-05-18 09:01:51 +0545

    Remove unnecessary files and dependencies

--- src/server/knexfile.js
+++ src/server/knexfile.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 /**
  * Database configuration.
  */
 module.exports = {
-  client: process.env.DB_CLIENT,
-  connection: {
-    filename: process.env.DB_FILE,
-  }
+    client: process.env.DB_CLIENT,
+    connection: {
+        filename: process.env.DB_FILE,
+    }
 };
