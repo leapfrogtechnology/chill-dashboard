433ae5b3aed74b74c6bd1e3ea60877984a22e948
romitamgai <rom_amgai@hotmail.com>
2017-05-26 18:05:40 +0545

    HOTFIX for deepak

--- src/server/services/status.js
+++ src/server/services/status.js
@@ -11,13 +11,23 @@ export async function getAllStatus() {
 }
 
 /**
+ * Fetch a service by its name.
+ *
+ * @param  {String} id
+ * @return {Promise}
+ */
+export async function fetchByName(name) {
+  return await new Status({ name }).groupBy('name').orderBy('created_at', 'DSC').fetch().attributes;
+}
+
+/**
  * Get a service status.
  *
  * @param  {string|Number}  id
  * @return {Promise}
  */
 export async function getStatus(id) {
-  return await new Status({id}).fetch()
+  return await new Status({ id }).fetch()
     .then(status => {
       if (!status) {
         throw new Boom.notFound('Service not found');
