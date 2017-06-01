b019c8a1105469ed874a67f999f0a17bac2d624e
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-06-01 19:42:20 +0545

    UI Improvements on the dashboard

--- src/app/components/dashboard/MainWrapper.js
+++ src/app/components/dashboard/MainWrapper.js
@@ -11,18 +11,19 @@ const MainWrapper = (props) => (
         <h1 className="page-header">Dashboard</h1>
       </div>
     </div>
+
     <div className="row">
-      <div className="col-xs-12 col-sm-6">
+      <div className="col-xs-12 col-sm-7">
+        <div className="row">
+          <ServicesTable services={props.services} />
+        </div>
+      </div>
+      <div className="col-xs-12 col-sm-5">
         <div className="row">
           <ServicesSummary totalRunning={props.totalRunning} totalStopped={props.totalStopped} />
           <SummaryPieChart totalRunning={props.totalRunning} totalStopped={props.totalStopped} />
         </div>
       </div>
-      <div className="col-xs-12 col-sm-6">
-      </div>
-    </div>
-    <div className="row">
-      <ServicesTable services={props.services} />
     </div>
   </div>
 );
--- src/app/components/dashboard/table/ServicesTable.js
+++ src/app/components/dashboard/table/ServicesTable.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import HttpStatus from 'http-status-codes';
+import ServiceStatus from './ServiceStatus';
 
-const ServicesTable = ({services}) => (
+const ServicesTable = ({ services }) => (
   <div className="col-lg-12 services-wrapper">
     <div className="panel panel-default">
-      <div className="panel-heading">List</div>
+      <div className="panel-heading">Services</div>
       <div className="panel-body">
         <div className="table-responsive">
           <table className="table table-bordered table-hover table-striped">
             <thead>
-            <tr>
-              <td>Service</td>
-              <td>Status</td>
-              <td>Last Active On</td>
-            </tr>
+              <tr>
+                <th className="service-name">Service</th>
+                <th>Status</th>
+                <th>Last Active On</th>
+              </tr>
             </thead>
             <tbody>
-            {services.map(service =>
-              <tr>
-                <td>{service.name}</td>
-                <td>{service.status === HttpStatus.OK ? <i className="fa fa-arrow-up" style={{color: 'green'}}/> :
-                  <i className="fa fa-arrow-down" style={{color: 'red'}}/>}</td>
-                <td>{service.createdAt}</td>
-              </tr>)}
+              {services.map(service =>
+                <tr>
+                  <td className="service-name">{service.name}</td>
+                  <td><ServiceStatus service={service} /></td>
+                  <td>{service.createdAt}</td>
+                </tr>
+              )}
             </tbody>
           </table>
         </div>
