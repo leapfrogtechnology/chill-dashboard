acef073202b80e928ae5c6aca4c7c0c1affce737
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-06-02 21:01:51 +0545

    Fix webpack production build issue

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,9 +1,7 @@
 const path = require('path');
 const dotenv = require('dotenv');
 const webpack = require('webpack');
-const resolve = require('path').resolve;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // Initialize environment variables
@@ -12,12 +10,12 @@ dotenv.config();
 const baseHref = process.env.BASE_HREF;
 
 module.exports = {
-  context: resolve(__dirname, 'src/app'),
+  context: path.resolve(__dirname, 'src/app'),
   entry: './index.js', // the entry point of our app
   output: {
-    publicPath: `/${process.env.BASE_HREF}`,
+    publicPath: `/`,
     chunkFilename: '[id].js',
-    path: resolve(__dirname, 'dist/app'),
+    path: path.resolve(__dirname, 'dist/app'),
     filename: 'js/bundle.[chunkhash].js' // the output bundle
   },
   module: {
@@ -41,7 +39,7 @@ module.exports = {
       },
       {
         test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        use: `file-loader?name=[name].[ext]&publicPath=/${baseHref}/fonts/&outputPath=/fonts/`
+        use: `file-loader`
       },
       {
         test: /\.scss$/,
@@ -52,7 +50,7 @@ module.exports = {
   plugins: [
     new ExtractTextPlugin('css/bundle.css'),
     new HtmlWebpackPlugin({
-      template: resolve(__dirname, 'public/index.html')
+      template: path.resolve(__dirname, 'public/index.html')
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
