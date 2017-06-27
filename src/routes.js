93b44f2664c9c40132021ea40b17f66234065445
pratishshr <vanroshr@gmail.com>
2017-06-27 09:02:06 +0545

    Update routes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import * as routes from './constants/routes';
-import Sidebar from './components/sidebar/Sidebar';
-import Dashboard from './components/dashboard/Dashboard';
+import config from './config';
+import routeConstants from './constants/routeConstants';
+
+import Dashboard from './components/Dashboard';
+import Header from './components/commons/Header';
+import Footer from './components/commons/Footer';
 
 const baseHref = process.env.BASE_HREF || '/';
 
 const Router = () => (
   <BrowserRouter basename={baseHref}>
-    <Switch>
-      <Route exact path={routes.DASHBOARD} component={Dashboard} />
-    </Switch>
+    <div>
+      <Header logoUrl={config.app.logoUrl} style={{ height: config.app.logoHeight }} />
+      <Switch>
+        <Route exact path={routeConstants.DASHBOARD} component={Dashboard} />
+      </Switch>
+      <Footer />
+    </div>
   </BrowserRouter>
 );
 
