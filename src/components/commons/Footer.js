271544113f0096d68fab7a315d749bf3fe03cd01
pratishshr <vanroshr@gmail.com>
2017-06-27 09:02:19 +0545

    Create header and footer

---
+++ src/components/commons/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="wrapper page-footer">
+      <div className="pull-right">
+        <span>
+          <a
+            target="_blank"
+            className="footer-text"
+            href="https://github.com/leapfrogtechnology/chill"
+          >
+            Powered By Chill
+          </a>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
