931123005e5422b485675ab4b124af6b824543b5
pratishshr <vanroshr@gmail.com>
2017-06-27 09:01:49 +0545

    Separate public imports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './public';
+
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
---
+++ src/public.js
@@ -0,0 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import '../public/css/main.css';
+import '../public/css/spinner.css';
