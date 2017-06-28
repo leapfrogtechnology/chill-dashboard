a21dacf867f06e1367811c5be919eef3a2383901
pratishshr <vanroshr@gmail.com>
2017-06-28 11:22:16 +0545

    Add tooltip

--- src/components/Dashboard/ServiceRow.js
+++ src/components/Dashboard/ServiceRow.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import moment from 'moment';
 import PropTypes from 'prop-types';
+import ToolTip from 'react-tooltip';
 
 import { isUp, getServiceParams } from '../../services/status';
 
 const ServiceRow = ({ service }) => {
+  let { id, name, createdAt } = service;
   let { message, className } = getServiceParams(isUp(service));
+
+  let tooltipId = `tooltip-${id}`;
 
   return (
     <li className="list-group-item">
-      <span>{service.name}</span>
+      <span>{name}</span>
       <span className={`list-item-right ${className}`}>{message}</span>
+      <a
+        data-tip
+        aria-hidden="true"
+        data-for={tooltipId}
+        className="list-item-tooltip glyphicon glyphicon-info-sign"
+      />
+
+      <ToolTip className="tooltip" place="left" id={tooltipId} type="dark">
+        <span>Since {moment(createdAt).fromNow()}</span>
+      </ToolTip>
     </li>
   );
 };
