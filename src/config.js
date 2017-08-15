f95281d14b5558b9785b1167cded02d0e74a972e
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-08-15 09:48:50 +0545

    Inject config via chill-core configuration

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
 export default {
   app: {
-    baseHref: process.env.BASE_HREF || '/',
-    title: process.env.APP_TITLE || 'Chill Dashboard',
-    logoUrl: process.env.APP_LOGO || require('../public/images/chill.png'),
-    logoHeight: process.env.APP_LOGO_HEIGHT || '80px'
+    baseHref: __INJECTED_CONFIG.baseHref || '/',
+    title: __INJECTED_CONFIG.title || 'Chill Dashboard',
+    logoUrl: __INJECTED_CONFIG.logo || require('../public/images/chill.png'),
+    logoHeight: __INJECTED_CONFIG.logoHeight || '80px'
   },
   api: {
-    baseUrl: process.env.API_ENDPOINT || 'http://localhost:8000',
+    baseUrl: __INJECTED_CONFIG.apiBaseUrl || 'http://localhost:8000',
     endpoints: {
       status: '/api/status'
     }
   },
   websocket: {
-    endpoint: process.env.WEBSOCKET_ENDPOINT || 'ws://localhost:8080',
+    endpoint: __INJECTED_CONFIG.websocketBaseUrl || 'ws://localhost:8080',
     reconnectTimeout: 5000
   }
 };
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,65 +1,75 @@
 const path = require('path');
-const dotenv = require('dotenv');
 const webpack = require('webpack');
+const { init } = require('chill-core');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-// Initialize environment variables
-dotenv.config();
+/**
+ * Configure Webpack.
+ *
+ * TODO: Extract common config.
+ *
+ * @returns {Promise}
+ */
+async function configureWebpack() {
+  const config = await init();
+  const baseHref = config.dashboard.baseHref;
 
-const baseHref = process.env.BASE_HREF;
-
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: './index.js', // the entry point of our app
-  output: {
-    publicPath: `/`,
-    chunkFilename: '[id].js',
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.[chunkhash].js' // the output bundle
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        use: `file-loader?name=[name].[ext]&publicPath=/${baseHref}/images/&outputPath=/images/`
-      },
-      {
-        test: /\.ico$/,
-        use: 'file-loader?name=[name].[ext]&publicPath=/images/&outputPath=/'
-      },
-      {
-        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        use: `file-loader`
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader', 'sass-loader'] })
-      }
+  return {
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.js', // the entry point of our app
+    output: {
+      publicPath: `/`,
+      chunkFilename: '[id].js',
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/bundle.[chunkhash].js' // the output bundle
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          use: `file-loader?name=[name].[ext]&publicPath=/${baseHref}/images/&outputPath=/images/`
+        },
+        {
+          test: /\.ico$/,
+          use: 'file-loader?name=[name].[ext]&publicPath=/images/&outputPath=/'
+        },
+        {
+          test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+          use: `file-loader`
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader', 'sass-loader'] })
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('css/bundle.css'),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html')
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+        '__INJECTED_CONFIG.logo': JSON.stringify(config.dashboard.logo),
+        '__INJECTED_CONFIG.title': JSON.stringify(config.dashboard.title),
+        '__INJECTED_CONFIG.baseHref': JSON.stringify(config.dashboard.baseHref),
+        '__INJECTED_CONFIG.apiBaseUrl': JSON.stringify(config.dashboard.apiBaseUrl),
+        '__INJECTED_CONFIG.logoHeight': JSON.stringify(config.dashboard.logoHeight),
+        '__INJECTED_CONFIG.websocketBaseUrl': JSON.stringify(config.dashboard.websocketBaseUrl)
+      }),
+      new webpack.optimize.UglifyJsPlugin()
     ]
-  },
-  plugins: [
-    new ExtractTextPlugin('css/bundle.css'),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html')
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-      'process.env.APP_LOGO': JSON.stringify(process.env.APP_LOGO),
-      'process.env.APP_TITLE': JSON.stringify(process.env.APP_TITLE),
-      'process.env.API_ENDPOINT': JSON.stringify(process.env.API_ENDPOINT),
-      'process.env.APP_LOGO_HEIGHT': JSON.stringify(process.env.APP_LOGO_HEIGHT),
-      'process.env.WEBSOCKET_ENDPOINT': JSON.stringify(process.env.WEBSOCKET_ENDPOINT)
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  ]
-};
+  };
+}
+
+module.exports = configureWebpack();
