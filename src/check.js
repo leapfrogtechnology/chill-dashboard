8f6090d8943af4ddd30e7397a404b3efdb8e1a8b
aishwaryashrestha11 <asworeya2shrestha@gmail.com>
2017-09-04 11:43:34 +0545

    meeting

---
+++ src/check.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class check extends Component {
+  render() {
+              console.log('hello');
+
+    return (
+      <div>
+        <h1>hellllo</h1>
+      </div>
+    );
+  }
+}
+
+export default check;
